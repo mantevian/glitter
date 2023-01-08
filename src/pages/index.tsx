f53b2b277a9c089b3c922584baa48d7f07fbc789
mantevian <zmantevian@gmail.com>
2023-01-08 16:38:46 +0300

    fix redirect from index to login (???)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import useRedirect from "../utils/hooks/useRedirect";
 export default function Home() {
 	const { sessionStatus, userData, userError } = useOwnSessionUser();
 
-	useRedirect(() => sessionStatus == "unauthenticated" || userError || (userData && !userData.username), "/login", []);
+	useRedirect(() => sessionStatus == "unauthenticated" || userError || (userData && !userData.username), "/login", [sessionStatus, userError, userData]);
 
 	return <HomeContent displayName={userData?.display_name.toString()} />;
 }
